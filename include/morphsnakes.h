f507baf45a865b051cbba9214d6e297578ecfdde
Pablo Márquez Neila <pmarquezneila@gmail.com>
2016-03-17 05:51:13 +0100

    C++ implementation: NDImage and iterators

--- include/morphsnakes.h
+++ include/morphsnakes.h
@@ -2,6 +2,11 @@
 #ifndef _MORPHSNAKES_H
 #define _MORPHSNAKES_H
 
+#include <map>
+#include <array>
+#include <vector>
+#include <numeric>
+
 namespace morphsnakes
 {
 
@@ -15,28 +20,247 @@ static const int curv_operator_2d[4][2] = {{0, 8},
 
 }
 
-template<class T, int D>
-class ndimage
+typedef int Position;
+
+template<int D>
+class Neighborhood;
+
+template<>
+class Neighborhood<2>
 {
 public:
+    static const int num_neighbors = 9;
+    static constexpr int offsets[num_neighbors][2] = {{-1, -1}, {-1, 0}, {-1, 1},
+                                                        {0, -1}, {0,0}, {0, 1},
+                                                        {1, -1}, {1, 0}, {1, 1}};
+    std::array<Position, num_neighbors> neighbors;
+
+public:
+    Neighborhood(const std::array<int, 2>& stride)
+    {
+        for(int i = 0; i < num_neighbors; ++i)
+            neighbors[i] = offsets[i][0] * stride[1] + offsets[i][1] * stride[0];
+    }
+    
+    std::array<Position, num_neighbors> getNeighbors(const Position& pos) const
+    {
+        std::array<Position, num_neighbors> res;
+        for(int i = 0; i < num_neighbors; ++i)
+            res[i] = neighbors[i] + pos;
+        
+        return res;
+    }
+};
+
+constexpr int Neighborhood<2>::offsets[Neighborhood<2>::num_neighbors][2];
+
+template<class T, int D>
+class NDImage;
+
+template<class T, int D>
+class NDImageIterator
+{
+public:
+    NDImageIterator(const NDImage<T, D>& image, bool atEnd=false)
+        : image(image)
+        , position(atEnd ? -1 : 0)
+    {
+        if(!atEnd)
+        {
+            cursor.fill(1);
+            position = std::inner_product(cursor.begin(), cursor.end(), image.stride.begin(), 0);
+        }
+    }
+    
+    NDImageIterator& operator++()
+    {
+        int i;
+        for(i = D - 1; i >= 0; --i)
+        {
+            if(cursor[i] < image.shape[i] - 2)
+            {
+                ++cursor[i];
+                break;
+            }
+            else
+                cursor[i] = 1;
+        }
+        
+        if(i == -1)
+            position = -1;
+        else
+            position = std::inner_product(cursor.begin(), cursor.end(), image.stride.begin(), 0);
+        
+        return *this;
+    }
+    
+    NDImageIterator operator++(int)
+    {
+        NDImageIterator aux(*this);
+        
+        ++(*this);
+        
+        return aux;
+    }
+    
+    Position operator*() {return position;}
+    
+    bool operator==(const NDImageIterator& rhs) const
+    {
+        return position == rhs.position;
+    }
+
+    bool operator!=(const NDImageIterator& rhs) const
+    {
+        return position != rhs.position;
+    }
+
+public:
+    const NDImage<T, D>& image;
+    Position position;
+    std::array<int, D> cursor;
+};
+
+template<class T, int D>
+class NDImage
+{
+public:
+    NDImage(T* data, const std::array<int, D>& shape, const std::array<int, D>& stride)
+        : data(data)
+        , data_bytes(reinterpret_cast<char*>(data))
+        , shape(shape)
+        , stride(stride)
+        , neighborhood(stride)
+    {}
+    
+    T& operator[](const Position& position)
+    {
+        return *reinterpret_cast<T*>(&data_bytes[position]);
+    }
+    
+    const T& operator[](const Position& position) const
+    {
+        return *reinterpret_cast<T*>(&data_bytes[position]);
+    }
+    
+    std::array<Position, Neighborhood<D>::num_neighbors> getNeighbors(const Position& position) const
+    {
+        return neighborhood.getNeighbors(position);
+    }
+    
+    NDImageIterator<T, D> begin() const
+    {
+        return NDImageIterator<T, D>(*this);
+    }
+    
+    NDImageIterator<T, D> end() const
+    {
+        return NDImageIterator<T, D>(*this, true);
+    }
+    
+public:
     T* data;
-    int shape[D];
-    int stride[D];
+    char* data_bytes;
+    std::array<int, D> shape;
+    std::array<int, D> stride;
+    const Neighborhood<D> neighborhood;
 };
 
 // Narrow band cell
-template<int D>
-class cell
+class Cell
 {
 public:
-    const int position[D];
+    Cell()
+        : toggle(false)
+    {}
+//    const Position<D> position;
+    bool toggle;
+//    int boundaries;
+//    bool dirty;
+};
+
+typedef std::map<Position, Cell > CellMap;
+
+template<class T, int D>
+CellMap createCellMap(const NDImage<T, D>& image)
+{
+    CellMap cellMap;
+    
+    for(auto position : image)
+    {
+        const T& val = image[position];
+        
+        for(auto n : image.getNeighbors(position))
+        {
+            if(image[n] != val)
+            {
+                cellMap[position] = Cell();
+                break;
+            }
+        }
+    }
+    
+    return cellMap;
+}
+
+template<int D>
+class NarrowBand
+{
+public:
+    typedef NDImage<int, D> Image;
+    
+    NarrowBand(Image& image)
+        : _image(image), _cells(createCellMap(image))
+    {}
+    
+    void toggleCell(const Position& position)
+    {
+        _cells[position].toggle = true;
+    }
+    
+    void flush()
+    {
+        CellMap updatedCells;
+        
+        auto cellIt = _cells.begin();
+        for(; cellIt != _cells.end(); ++cellIt)
+        {
+            const Position& position = cellIt->first;
+            if(!cellIt->second.toggle)
+                continue;
+            
+            _image[position] = !_image[position];
+            cellIt->second.toggle = false;
+            
+            auto neighbors = _image.neighborhood.getNeighbors(position);
+            for(auto n : neighbors)
+                updatedCells[n] = Cell();
+        }
+        
+        _cells.insert(updatedCells.begin(), updatedCells.end());
+    }
+    
+    void prune()
+    {
+        auto cellIt = _cells.begin();
+        for(; cellIt != _cells.end(); ++cellIt)
+        {
+            Position position = cellIt->first;
+            auto val = _image[position];
+            
+            // for(auto n : _image.getNeighbors(position))
+            // {
+            //     if(_image[n] != val)
+            //         toRemove.insert(position);
+            // }
+        }
+    }
+    
+    const CellMap& getCellMap() const {return _cells;}
     
 private:
-    ndimage<bool, D>* image;
-    
-    bool toggle;
-    int boundaries;
-    bool dirty;
+    Image& _image;
+    CellMap _cells;
 };
 
 template<int D>
@@ -48,11 +272,11 @@ class embedding_function<2>
     static const int num_neighbors = 9;
     
 private:
-    ndimage<bool, 2>* image;
+    NDImage<bool, 2>* image;
     int neighborhood[num_neighbors];
     
 public:
-    embedding_function(ndimage<bool, 2>* _image)
+    embedding_function(NDImage<bool, 2>* _image)
         : image(_image)
     {
         static const int offsets[][2] = {{-1, -1}, {-1, 0}, {-1, 1},
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,7 +1,41 @@
 
+#include <algorithm>
+#include <iostream>
+
 #include "morphsnakes.h"
 
 int main()
 {
+    int data[100];
+    std::fill(data, data+100, 0);
+    
+    std::array<int, 2> shape = {10, 10};
+    std::array<int, 2> strides = {sizeof(int) * 10, sizeof(int)};
+    
+    data[45] = 1;
+    
+    morphsnakes::NDImage<int, 2> image(data, shape, strides);
+    
+    // for(auto aux : image)
+    // {
+    //     std::cout << image[aux] << std::endl;
+    //     for(auto n : image.getNeighbors(aux))
+    //         std::cout << aux << " " << n << std::endl;
+    // }
+    
+    // morphsnakes::CellMap cellMap = createCellMap(image);
+    // std::cout << cellMap << std::endl;
+    morphsnakes::NarrowBand<2> narrowBand(image);
+    narrowBand.toggleCell(180);
+    for(auto c : narrowBand.getCellMap())
+    {
+        std::cout << c.first << " " << c.second.toggle << std::endl;
+    }
+    narrowBand.flush();
+    for(auto c : narrowBand.getCellMap())
+    {
+        std::cout << c.first << " " << c.second.toggle << std::endl;
+    }
+    
     return 0;
 }
