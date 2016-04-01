4d4345b7c5a0b2808a7cd95053ed5d1cae109035
Pablo Márquez Neila <pmarquezneila@gmail.com>
2016-04-01 21:10:58 +0200

    Dilation, erosion and curvature for 2D and 3D

--- include/morphsnakes.h
+++ include/morphsnakes.h
@@ -6,12 +6,22 @@
 #include <array>
 #include <vector>
 #include <numeric>
+#include <iostream>
 
 namespace morphsnakes
 {
 
+template<size_t D>
+std::array<int, D> operator+(const std::array<int, D>& a, const std::array<int, D>& b)
+{
+    std::array<int, D> res;
+    for(int i = 0; i < D; ++i)
+        res[i] = a[i] + b[i];
+    return res;
+}
+
 // typedef int Position;
-template<int D>
+template<size_t D>
 class Position
 {
 public:
@@ -36,7 +46,13 @@ public:
     }
 };
 
-template<int D>
+std::ostream& operator<<(std::ostream& ostr, const morphsnakes::Position<2>& pos)
+{
+    ostr << pos.offset << "(" << pos.coord[0] << ", " << pos.coord[1] << ")";
+    return ostr;
+}
+
+template<size_t D>
 bool isBoundary(const std::array<int, D>& coords, const std::array<int, D>& shape)
 {
     for(int i = 0; i < D; ++i)
@@ -47,13 +63,13 @@ bool isBoundary(const std::array<int, D>& coords, const std::array<int, D>& shap
     return false;
 }
 
-template<int D>
+template<size_t D>
 bool isBoundary(const Position<D>& position, const std::array<int, D>& shape)
 {
     return isBoundary<D>(position.coord, shape);
 }
 
-template<int D>
+template<size_t D>
 std::array<int, D> offsetToCoord(int offset, const std::array<int, D>& stride)
 {
     std::array<int, D> res;
@@ -62,13 +78,13 @@ std::array<int, D> offsetToCoord(int offset, const std::array<int, D>& stride)
     return res;
 }
 
-template<int D>
+template<size_t D>
 int coordToOffset(const std::array<int, D>& coord, const std::array<int, D>& stride)
 {
     return std::inner_product(coord.begin(), coord.end(), stride.begin(), 0);
 }
 
-template<int D>
+template<size_t D>
 class NeighborOffsets;
 
 template<>
@@ -92,16 +108,51 @@ public:
     NeighborOffsets(const std::array<int, 2>& stride)
     {
         for(int i = 0; i < num_neighbors; ++i)
-            linear_offsets[i] = coord_offsets[i][0] * stride[1] + coord_offsets[i][1] * stride[0];
+            linear_offsets[i] = coord_offsets[i][1] * stride[1] + coord_offsets[i][0] * stride[0];
     }
 };
 
 constexpr NeighborOffsets<2>::CoordOffsets NeighborOffsets<2>::coord_offsets;
 
-template<class T, int D>
+template<>
+class NeighborOffsets<3>
+{
+public:
+    static const int num_neighbors = 27;
+    
+    typedef Position<3>::Coord Coord;
+    typedef std::array<Coord, num_neighbors> CoordOffsets;
+    typedef std::array<int, num_neighbors> LinearOffsets;
+    
+    static constexpr CoordOffsets coord_offsets = {
+        {{-1, -1, -1}, {-1, -1, 0}, {-1, -1, 1},
+         {-1, 0, -1}, {-1, 0, 0}, {-1, 0, 1},
+         {-1, 1, -1}, {-1, 1, 0}, {-1, 1, 1},
+         {0, -1, -1}, {0, -1, 0}, {0, -1, 1},
+         {0, 0, -1}, {0, 0, 0}, {0, 0, 1},
+         {0, 1, -1}, {0, 1, 0}, {0, 1, 1},
+         {1, -1, -1}, {1, -1, 0}, {1, -1, 1},
+         {1, 0, -1}, {1, 0, 0}, {1, 0, 1},
+         {1, 1, -1}, {1, 1, 0}, {1, 1, 1}}
+    };
+    LinearOffsets linear_offsets;
+
+public:
+    NeighborOffsets(const std::array<int, 2>& stride)
+    {
+        for(int i = 0; i < num_neighbors; ++i)
+            linear_offsets[i] = coord_offsets[i][2] * stride[2] +
+                                coord_offsets[i][1] * stride[1] +
+                                coord_offsets[i][0] * stride[0];
+    }
+};
+
+constexpr NeighborOffsets<3>::CoordOffsets NeighborOffsets<3>::coord_offsets;
+
+template<class T, size_t D>
 class NDImage;
 
-template<int D>
+template<size_t D>
 class NDImageIterator
 {
 public:
@@ -166,7 +217,7 @@ public:
     Position<D> position;
 };
 
-template<int D>
+template<size_t D>
 class NeighborhoodIterator
 {
 public:
@@ -201,14 +252,8 @@ public:
     
     Position<D> operator*()
     {
-        Coord new_coord;
-        
-        const Coord& coord_offset = *coord_iterator;
-        for(int i = 0; i < D; ++i)
-            new_coord[i] = center.coord[i] + coord_offset[i];
-        
+        Coord new_coord = center.coord + *coord_iterator;
         int new_offset = center.offset + *linear_iterator;
-        
         return Position<D>(new_coord, new_offset);
     }
     
@@ -228,7 +273,7 @@ private:
     LinearOffsetsIt linear_iterator;
 };
 
-template<int D>
+template<size_t D>
 class Neighborhood
 {
 public:
@@ -236,6 +281,14 @@ public:
         : center(center)
         , offsets(offsets)
     {}
+    
+    Position<D> getNeighbor(int index) const
+    {
+        const typename Position<D>::Coord& coord_offset = offsets.coord_offsets[index];
+        int linear_offset = offsets.linear_offsets[index];
+        
+        return Position<D>(center.coord + coord_offset, center.offset + linear_offset);
+    }
     
     NeighborhoodIterator<D> begin()
     {
@@ -252,7 +305,7 @@ private:
     const NeighborOffsets<D>& offsets;
 };
 
-template<class T, int D>
+template<class T, size_t D>
 class NDImage
 {
 public:
@@ -317,7 +370,7 @@ public:
     bool toggle;
 };
 
-template<int D>
+template<size_t D>
 class NarrowBand
 {
 public:
@@ -420,16 +473,63 @@ private:
     CellMap _cells;
 };
 
-static const int curv_operator_2d[4][2] = {{0, 8},
-                                           {1, 7},
-                                           {2, 6},
-                                           {3, 5}};
+// Descriptors of morphological operators
+
+template<size_t num_elements, size_t num_neighbors>
+using OperatorDescriptor = std::array<std::array<int, num_neighbors>, num_elements>;
+
+template<size_t D>
+struct Operator;
+
+template<>
+struct Operator<2>
+{
+    static constexpr OperatorDescriptor<4, 2> curvature  = {
+        {{0, 8},
+        {1, 7},
+        {2, 6},
+        {3, 5}}
+    };
+
+    static constexpr OperatorDescriptor<1, 8> dilate_erode = {
+        {{0, 1, 2, 3, 5, 6, 7, 8}}
+    };
+};
+
+constexpr OperatorDescriptor<4, 2> Operator<2>::curvature;
+constexpr OperatorDescriptor<1, 8> Operator<2>::dilate_erode;
+
+template<>
+struct Operator<3>
+{
+    static constexpr OperatorDescriptor<9, 8> curvature  = {
+        {{6, 7, 8, 12, 14, 18, 19, 20},
+        {9, 10, 11, 12, 14, 15, 16, 17},
+        {0, 1, 2, 12, 14, 24, 25, 26},
+        {0, 4, 8, 9, 17, 18, 22, 26},
+        {3, 4, 5, 12, 14, 21, 22, 23},
+        {2, 4, 6, 11, 15, 20, 22, 24},
+        {2, 5, 8, 10, 16, 18, 21, 24},
+        {1, 4, 7, 10, 16, 19, 22, 25},
+        {0, 3, 6, 10, 16, 20, 23, 26}
+        }
+    };
+
+    static constexpr OperatorDescriptor<1, 26> dilate_erode = {
+        {{0, 1, 2, 3, 4, 5, 6, 7, 8,
+          9,10,11,12,14,15,16,17,
+         18,19,20,21,22,23,24,25,26}}
+    };
+};
+
+constexpr OperatorDescriptor<9, 8> Operator<3>::curvature;
+constexpr OperatorDescriptor<1, 26> Operator<3>::dilate_erode;
 
 /**
  * Morphological operator acting over a narrow band.
  */
-template<class M, int D>
-void morph_op(const M& op, bool sup_inf, NarrowBand<D>& narrowBand)
+template<class M, size_t D>
+void morph_op(const M& op, bool inf_sup, NarrowBand<D>& narrowBand)
 {
     typedef typename NarrowBand<D>::CellMap CellMap;
     typedef typename NarrowBand<D>::Image Image;
@@ -437,18 +537,66 @@ void morph_op(const M& op, bool sup_inf, NarrowBand<D>& narrowBand)
     const CellMap& cellMap = narrowBand.getCellMap();
     const Image& image = narrowBand.getImage();
     
-    for(auto cell : cellMap)
+    for(auto& cell : cellMap)
     {
-        auto val = image[cell.first];
+        auto& val = image[cell.first];
         
         // If sup_inf and val is 0 or inf_sup and val is 1, then no change is possible.
-        if(val != sup_inf)
+        if(val == inf_sup)
             continue;
         
         const Neighborhood<D>& neighborhood = image.neighborhood(cell.first);
+        bool shouldToggle = true;
+        for(auto& elem : op)
+        {
+            bool active_element = false;
+            for(auto& index : elem)
+            {
+                const Position<D>& n = neighborhood.getNeighbor(index);
+                auto& val = image[n];
+                
+                if(val == inf_sup)
+                {
+                    active_element = true;
+                    break;
+                }
+            }
+            
+            if(!active_element)
+            {
+                shouldToggle = false;
+                break;
+            }
+        }
         
+        if(shouldToggle)
+            narrowBand.toggleCell(cell.first);
     }
 }
+
+// Common morphological operators: dilation, erosion and curvature
+
+template<size_t D>
+void dilate(NarrowBand<D>& narrowBand)
+{
+    morph_op(Operator<D>::dilate_erode, true, narrowBand);
+}
+
+template<size_t D>
+void erode(NarrowBand<D>& narrowBand)
+{
+    morph_op(Operator<D>::dilate_erode, false, narrowBand);
+}
+
+template<size_t D>
+void curv(bool inf_sup, NarrowBand<D>& narrowBand)
+{
+    morph_op(Operator<D>::curvature, inf_sup, narrowBand);
+}
+
+// Image attachment
+
+
 
 }
 
