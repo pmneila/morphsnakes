8b530553f1ba6a7086a6c4c0f6a7c68fe24c6f44
Pablo Márquez Neila <pmarquezneila@gmail.com>
2018-10-06 17:13:18 +0200

    examples.py updated to the new API

--- examples.py
+++ examples.py
@@ -1,17 +1,18 @@
+
 import os
 import logging
 
+import numpy as np
+from imageio import imread
 import matplotlib
+from matplotlib import pyplot as plt
+
+import morphsnakes as ms
+
 # in case you are running on machine without display, e.g. server
 if os.environ.get('DISPLAY', '') == '':
     logging.warning('No display found. Using non-interactive Agg backend.')
     matplotlib.use('Agg')
-
-import numpy as np
-from scipy.misc import imread
-from matplotlib import pyplot as plt
-
-import morphsnakes
 
 PATH_IMG_NODULE = 'images/mama07ORI.bmp'
 PATH_IMG_STARFISH = 'images/seastar2.png'
@@ -19,89 +20,213 @@ PATH_IMG_LAKES = 'images/lakes3.jpg'
 PATH_ARRAY_CONFOCAL = 'images/confocal.npy'
 
 
+def visual_callback_2d(background, fig=None):
+    """
+    Returns a callback than can be passed as the argument `iter_callback`
+    of `morphological_geodesic_active_contour` and
+    `morphological_chan_vese` for visualizing the evolution
+    of the levelsets. Only works for 2D images.
+    
+    Parameters
+    ----------
+    background : (M, N) array
+        Image to be plotted as the background of the visual evolution.
+    fig : matplotlib.figure.Figure
+        Figure where results will be drawn. If not given, a new figure
+        will be created.
+    
+    Returns
+    -------
+    callback : Python function
+        A function that receives a levelset and updates the current plot
+        accordingly. This can be passed as the `iter_callback` argument of
+        `morphological_geodesic_active_contour` and
+        `morphological_chan_vese`.
+    
+    """
+    
+    # Prepare the visual environment.
+    if fig is None:
+        fig = plt.figure()
+    fig.clf()
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax1.imshow(background, cmap=plt.cm.gray)
+
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax_u = ax2.imshow(np.zeros_like(background), vmin=0, vmax=1)
+    plt.pause(0.001)
+
+    def callback(levelset):
+        
+        if ax1.collections:
+            del ax1.collections[0]
+        ax1.contour(levelset, [0.5], colors='r')
+        ax_u.set_data(levelset)
+        fig.canvas.draw()
+        plt.pause(0.001)
+
+    return callback
+
+
+def visual_callback_3d(fig=None, plot_each=1):
+    """
+    Returns a callback than can be passed as the argument `iter_callback`
+    of `morphological_geodesic_active_contour` and
+    `morphological_chan_vese` for visualizing the evolution
+    of the levelsets. Only works for 3D images.
+    
+    Parameters
+    ----------
+    fig : matplotlib.figure.Figure
+        Figure where results will be drawn. If not given, a new figure
+        will be created.
+    plot_each : positive integer
+        The plot will be updated once every `plot_each` calls to the callback
+        function.
+    
+    Returns
+    -------
+    callback : Python function
+        A function that receives a levelset and updates the current plot
+        accordingly. This can be passed as the `iter_callback` argument of
+        `morphological_geodesic_active_contour` and
+        `morphological_chan_vese`.
+    
+    """
+    
+    from mpl_toolkits.mplot3d import Axes3D
+    # PyMCubes package is required for `visual_callback_3d`
+    try:
+        import mcubes
+    except ImportError:
+        raise ImportError("PyMCubes is required for 3D `visual_callback_3d`")
+    
+    # Prepare the visual environment.
+    if fig is None:
+        fig = plt.figure()
+    fig.clf()
+    ax = fig.add_subplot(111, projection='3d')
+    plt.pause(0.001)
+    
+    counter = -1
+
+    def callback(levelset):
+
+        nonlocal counter
+        counter += 1
+        if (counter % plot_each) != 0:
+            return
+
+        if ax.collections:
+            del ax.collections[0]
+
+        coords, triangles = mcubes.marching_cubes(levelset, 0.5)
+        ax.plot_trisurf(coords[:, 0], coords[:, 1], coords[:, 2],
+                        triangles=triangles)
+        plt.pause(0.1)
+
+    return callback
+
+
 def rgb2gray(img):
     """Convert a RGB image to gray scale."""
-    return 0.2989 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]
+    return 0.2989 * img[..., 0] + 0.587 * img[..., 1] + 0.114 * img[..., 2]
 
 
-def circle_levelset(shape, center, sqradius, scalerow=1.0):
-    """Build a binary function with a circle as the 0.5-levelset."""
-    grid = np.mgrid[list(map(slice, shape))].T - center
-    phi = sqradius - np.sqrt(np.sum((grid.T) ** 2, 0))
-    u = np.float_(phi > 0)
-    return u
-
-
-def test_nodule():
-    logging.info('running: test_nodule...')
+def example_nodule():
+    logging.info('Running: example_nodule...')
+    
     # Load the image.
     img = imread(PATH_IMG_NODULE)[..., 0] / 255.0
     
     # g(I)
-    gI = morphsnakes.gborders(img, alpha=1000, sigma=5.48)
+    gimg = ms.inverse_gaussian_gradient(img, alpha=1000, sigma=5.48)
     
-    # Morphological GAC. Initialization of the level-set.
-    mgac = morphsnakes.MorphGAC(gI, smoothing=1, threshold=0.31, balloon=1)
-    mgac.levelset = circle_levelset(img.shape, (100, 126), 20)
+    # Initialization of the level-set.
+    init_ls = ms.circle_level_set(img.shape, (100, 126), 20)
     
-    # Visual evolution.
-    fig = plt.figure()
-    morphsnakes.evolve_visual(mgac, fig, num_iters=45, background=img)
+    # Callback for visual plotting
+    callback = visual_callback_2d(img)
+    
+    # Morphological GAC. 
+    ms.morphological_geodesic_active_contour(gimg, iterations=45, 
+                                             init_level_set=init_ls,
+                                             smoothing=1, threshold=0.31,
+                                             balloon=1, iter_callback=callback)
 
 
-def test_starfish():
-    logging.info('running: test_starfish...')
+def example_starfish():
+    logging.info('Running: example_starfish...')
+    
     # Load the image.
     imgcolor = imread(PATH_IMG_STARFISH) / 255.0
     img = rgb2gray(imgcolor)
     
     # g(I)
-    gI = morphsnakes.gborders(img, alpha=1000, sigma=2)
+    gimg = ms.inverse_gaussian_gradient(img, alpha=1000, sigma=2)
     
-    # Morphological GAC. Initialization of the level-set.
-    mgac = morphsnakes.MorphGAC(gI, smoothing=2, threshold=0.3, balloon=-1)
-    mgac.levelset = circle_levelset(img.shape, (163, 137), 135, scalerow=0.75)
+    # Initialization of the level-set.
+    init_ls = ms.circle_level_set(img.shape, (163, 137), 135)
     
-    # Visual evolution.
-    fig = plt.figure()
-    morphsnakes.evolve_visual(mgac, fig, num_iters=100, background=imgcolor)
+    # Callback for visual plotting
+    callback = visual_callback_2d(imgcolor)
+    
+    # Morphological GAC. 
+    ms.morphological_geodesic_active_contour(gimg, iterations=100, 
+                                             init_level_set=init_ls,
+                                             smoothing=2, threshold=0.3,
+                                             balloon=-1, iter_callback=callback)
 
 
-def test_lakes():
-    logging.info('running: test_lakes...')
+def example_lakes():
+    logging.info('Running: example_lakes...')
+    
     # Load the image.
     imgcolor = imread(PATH_IMG_LAKES)/255.0
     img = rgb2gray(imgcolor)
     
     # MorphACWE does not need g(I)
     
-    # Morphological ACWE. Initialization of the level-set.
-    macwe = morphsnakes.MorphACWE(img, smoothing=3, lambda1=1, lambda2=1)
-    macwe.levelset = circle_levelset(img.shape, (80, 170), 25)
+    # Initialization of the level-set.
+    init_ls = ms.circle_level_set(img.shape, (80, 170), 25)
     
-    # Visual evolution.
-    fig = plt.figure()
-    morphsnakes.evolve_visual(macwe, fig, num_iters=200, background=imgcolor)
+    # Callback for visual plotting
+    callback = visual_callback_2d(imgcolor)
+
+    # Morphological Chan-Vese (or ACWE)
+    ms.morphological_chan_vese(img, iterations=200,
+                               init_level_set=init_ls,
+                               smoothing=3, lambda1=1, lambda2=1,
+                               iter_callback=callback)
 
 
-def sample_confocal3d():
-    logging.info('running: sample_confocal3d...')
+def example_confocal3d():
+    logging.info('Running: example_confocal3d...')
+    
     # Load the image.
     img = np.load(PATH_ARRAY_CONFOCAL)
     
-    # Morphological ACWE. Initialization of the level-set.
-    macwe = morphsnakes.MorphACWE(img, smoothing=1, lambda1=1, lambda2=2)
-    macwe.levelset = circle_levelset(img.shape, (30, 50, 80), 25)
-    
-    # Visual evolution.
-    morphsnakes.evolve_visual3d(macwe, num_iters=150,
-                                animate_ui=False, animate_delay=10)
+    # Initialization of the level-set.
+    init_ls = ms.circle_level_set(img.shape, (30, 50, 80), 25)
 
+    # Callback for visual plotting
+    callback = visual_callback_3d(plot_each=20)
+
+    # Morphological Chan-Vese (or ACWE)
+    ms.morphological_chan_vese(img, iterations=150,
+                               init_level_set=init_ls,
+                               smoothing=1, lambda1=1, lambda2=2,
+                               iter_callback=callback)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    test_nodule()
-    test_starfish()
-    test_lakes()
-    sample_confocal3d()
+    example_nodule()
+    example_starfish()
+    example_lakes()
+    
+    # Uncomment the following line to see a 3D example
+    # This is skipped by default since mplot3d is VERY slow plotting 3d meshes
+    # example_confocal3d()
+    
+    logging.info("Done.")
     plt.show()
