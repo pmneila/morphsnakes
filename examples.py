46b4ea2c548cfb86a3e4a9401ec42a0219277fcf
Pablo Márquez Neila <pmarquezneila@gmail.com>
2020-11-30 06:08:38 +0100

    Style fixes

--- examples.py
+++ examples.py
@@ -28,7 +28,7 @@ def visual_callback_2d(background, fig=None):
     of `morphological_geodesic_active_contour` and
     `morphological_chan_vese` for visualizing the evolution
     of the levelsets. Only works for 2D images.
-    
+
     Parameters
     ----------
     background : (M, N) array
@@ -36,7 +36,7 @@ def visual_callback_2d(background, fig=None):
     fig : matplotlib.figure.Figure
         Figure where results will be drawn. If not given, a new figure
         will be created.
-    
+
     Returns
     -------
     callback : Python function
@@ -44,9 +44,9 @@ def visual_callback_2d(background, fig=None):
         accordingly. This can be passed as the `iter_callback` argument of
         `morphological_geodesic_active_contour` and
         `morphological_chan_vese`.
-    
+
     """
-    
+
     # Prepare the visual environment.
     if fig is None:
         fig = plt.figure()
@@ -59,7 +59,7 @@ def visual_callback_2d(background, fig=None):
     plt.pause(0.001)
 
     def callback(levelset):
-        
+
         if ax1.collections:
             del ax1.collections[0]
         ax1.contour(levelset, [0.5], colors='r')
@@ -76,7 +76,7 @@ def visual_callback_3d(fig=None, plot_each=1):
     of `morphological_geodesic_active_contour` and
     `morphological_chan_vese` for visualizing the evolution
     of the levelsets. Only works for 3D images.
-    
+
     Parameters
     ----------
     fig : matplotlib.figure.Figure
@@ -85,7 +85,7 @@ def visual_callback_3d(fig=None, plot_each=1):
     plot_each : positive integer
         The plot will be updated once every `plot_each` calls to the callback
         function.
-    
+
     Returns
     -------
     callback : Python function
@@ -93,23 +93,23 @@ def visual_callback_3d(fig=None, plot_each=1):
         accordingly. This can be passed as the `iter_callback` argument of
         `morphological_geodesic_active_contour` and
         `morphological_chan_vese`.
-    
+
     """
-    
+
     from mpl_toolkits.mplot3d import Axes3D
     # PyMCubes package is required for `visual_callback_3d`
     try:
         import mcubes
     except ImportError:
         raise ImportError("PyMCubes is required for 3D `visual_callback_3d`")
-    
+
     # Prepare the visual environment.
     if fig is None:
         fig = plt.figure()
     fig.clf()
     ax = fig.add_subplot(111, projection='3d')
     plt.pause(0.001)
-    
+
     counter = [-1]
 
     def callback(levelset):
@@ -136,21 +136,21 @@ def rgb2gray(img):
 
 def example_nodule():
     logging.info('Running: example_nodule (MorphGAC)...')
-    
+
     # Load the image.
     img = imread(PATH_IMG_NODULE)[..., 0] / 255.0
-    
+
     # g(I)
     gimg = ms.inverse_gaussian_gradient(img, alpha=1000, sigma=5.48)
-    
+
     # Initialization of the level-set.
     init_ls = ms.circle_level_set(img.shape, (100, 126), 20)
-    
+
     # Callback for visual plotting
     callback = visual_callback_2d(img)
-    
-    # MorphGAC. 
-    ms.morphological_geodesic_active_contour(gimg, iterations=45, 
+
+    # MorphGAC.
+    ms.morphological_geodesic_active_contour(gimg, iterations=45,
                                              init_level_set=init_ls,
                                              smoothing=1, threshold=0.31,
                                              balloon=1, iter_callback=callback)
@@ -158,22 +158,22 @@ def example_nodule():
 
 def example_starfish():
     logging.info('Running: example_starfish (MorphGAC)...')
-    
+
     # Load the image.
     imgcolor = imread(PATH_IMG_STARFISH) / 255.0
     img = rgb2gray(imgcolor)
-    
+
     # g(I)
     gimg = ms.inverse_gaussian_gradient(img, alpha=1000, sigma=2)
-    
+
     # Initialization of the level-set.
     init_ls = ms.circle_level_set(img.shape, (163, 137), 135)
-    
+
     # Callback for visual plotting
     callback = visual_callback_2d(imgcolor)
-    
-    # MorphGAC. 
-    ms.morphological_geodesic_active_contour(gimg, iterations=100, 
+
+    # MorphGAC.
+    ms.morphological_geodesic_active_contour(gimg, iterations=100,
                                              init_level_set=init_ls,
                                              smoothing=2, threshold=0.3,
                                              balloon=-1, iter_callback=callback)
@@ -181,21 +181,21 @@ def example_starfish():
 
 def example_coins():
     logging.info('Running: example_coins (MorphGAC)...')
-    
+
     # Load the image.
     img = imread(PATH_IMG_COINS) / 255.0
-    
+
     # g(I)
     gimg = ms.inverse_gaussian_gradient(img)
-    
+
     # Manual initialization of the level set
     init_ls = np.zeros(img.shape, dtype=np.int8)
     init_ls[10:-10, 10:-10] = 1
-    
+
     # Callback for visual plotting
     callback = visual_callback_2d(img)
-    
-    # MorphGAC. 
+
+    # MorphGAC.
     ms.morphological_geodesic_active_contour(gimg, 230, init_ls,
                                              smoothing=1, threshold=0.69,
                                              balloon=-1, iter_callback=callback)
@@ -203,16 +203,16 @@ def example_coins():
 
 def example_lakes():
     logging.info('Running: example_lakes (MorphACWE)...')
-    
+
     # Load the image.
     imgcolor = imread(PATH_IMG_LAKES)/255.0
     img = rgb2gray(imgcolor)
-    
+
     # MorphACWE does not need g(I)
-    
+
     # Initialization of the level-set.
     init_ls = ms.circle_level_set(img.shape, (80, 170), 25)
-    
+
     # Callback for visual plotting
     callback = visual_callback_2d(imgcolor)
 
@@ -228,7 +228,7 @@ def example_camera():
     Example with `morphological_chan_vese` with using the default
     initialization of the level-set.
     """
-    
+
     logging.info('Running: example_camera (MorphACWE)...')
 
     # Load the image.
@@ -245,10 +245,10 @@ def example_camera():
 
 def example_confocal3d():
     logging.info('Running: example_confocal3d (MorphACWE)...')
-    
+
     # Load the image.
     img = np.load(PATH_ARRAY_CONFOCAL)
-    
+
     # Initialization of the level-set.
     init_ls = ms.circle_level_set(img.shape, (30, 50, 80), 25)
 
@@ -261,6 +261,7 @@ def example_confocal3d():
                                smoothing=1, lambda1=1, lambda2=2,
                                iter_callback=callback)
 
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     example_nodule()
@@ -268,10 +269,10 @@ if __name__ == '__main__':
     example_coins()
     example_lakes()
     example_camera()
-    
+
     # Uncomment the following line to see a 3D example
     # This is skipped by default since mplot3d is VERY slow plotting 3d meshes
     # example_confocal3d()
-    
+
     logging.info("Done.")
     plt.show()
